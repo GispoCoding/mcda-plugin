e1b33013a4040c1e62229d158b519344a0787a7c
lehtojaa <jaakko.lehto@gispo.fi>
2022-05-16 23:53:29 +0300

    Populate risk layer grid

--- rasterriskassessmentplugin/ui/hazard_panel.py
+++ rasterriskassessmentplugin/ui/hazard_panel.py
@@ -1,6 +1,6 @@
 import logging
 
-from qgis.PyQt.QtWidgets import QDialog, QProgressBar
+from qgis.PyQt.QtWidgets import QDialog, QProgressBar, QGridLayout, QLabel
 from qgis.gui import QgsMapLayerComboBox, QgsSpinBox
 from qgis.core import QgsFieldProxyModel, QgsMapLayerProxyModel
 
@@ -25,6 +25,10 @@ class HazardRiskIndexPanel(BasePanel):
         self.dlg.hri_map_layer_cmb_bx_boundaries: QgsMapLayerComboBox
         self.dlg.hri_map_layer_cmb_bx_schools: QgsMapLayerComboBox
         self.dlg.hri_progress_bar: QProgressBar
+        self.dlg.hri_risk_layer_gridlayout: QGridLayout
+
+        # self.dlg.hri_risk_layer_gridlayout.setRowCount(1)
+        # self.dlg.hri_risk_layer_gridlayout.setColumnCount(3)
 
         # self.dlg.hri_progress_bar.setMinimum(0)
         self.dlg.hri_progress_bar.setValue(0)
@@ -37,7 +41,7 @@ class HazardRiskIndexPanel(BasePanel):
         self.dlg.hri_spn_bx_number_of_hazards: QgsSpinBox
         self.dlg.hri_spn_bx_number_of_hazards.setClearValue(1)
         self.dlg.hri_spn_bx_number_of_hazards.clear()
-        # self.dlg.hri_spn_bx_value = self.dlg.hri_spn_bx_number_of_hazards.value()
+
         self.dlg.hri_spn_bx_number_of_hazards.valueChanged.connect(
             self.__set_hri_risk_layer_grid
         )
@@ -56,6 +60,35 @@ class HazardRiskIndexPanel(BasePanel):
     def __set_hri_risk_layer_grid(self, nr_of_risks):
 
         print(nr_of_risks)
+
+        """self.stored_queries: List[StoredQuery] = self.sq_factory.list_queries()  # type: ignore # noqa E501
+        self.tbl_wdgt_stored_queries.setRowCount(len(self.stored_queries))
+        self.tbl_wdgt_stored_queries.setColumnCount(3)"""
+
+        for i in range(nr_of_risks):
+            self.dlg.hri_risk_layer_gridlayout.addWidget(
+                QLabel("Layer {}".format(i + 1), self.dlg), i, 0
+            )
+            self.dlg.hri_risk_layer_gridlayout.addWidget(
+                QgsMapLayerComboBox(),
+                i,
+                1,
+            )
+            """self.dlg.hri_risk_layer_gridlayout.addWidget(
+                QLabel("Layer {}".format(i + 1), self.dlg), i, 1
+            )"""
+            self.dlg.hri_risk_layer_gridlayout.addWidget(
+                QLabel("Layer1", self.dlg), i, 2
+            )
+            # pass
+            """self.tbl_wdgt_stored_queries.setItem(i, 0, QTableWidgetItem(sq.title))
+            abstract_item = QTableWidgetItem(sq.abstract)
+            abstract_item.setToolTip(sq.abstract)
+            self.tbl_wdgt_stored_queries.setItem(i, 1, abstract_item)
+            id_item = QTableWidgetItem(sq.id)
+            id_item.setToolTip(sq.id)
+            self.tbl_wdgt_stored_queries.setItem(i, 2, id_item)"""
+
         print("risks have changed")
 
     def __close_dialog(self):
